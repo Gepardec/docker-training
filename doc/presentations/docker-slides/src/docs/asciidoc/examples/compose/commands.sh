0068acce1df3cfc3adfe5016f4ea47298db1d6c1
het <thomas.herzog@gepardec.com>
2018-11-14 17:00:28 +0100

    Further dev on compose example

---
+++ doc/presentations/docker-slides/src/docs/asciidoc/examples/compose/commands.sh
@@ -0,0 +1,33 @@
+# tag::backup[]
+# 1. Stop/Delete the Docker Containers
+docker-compose down
+
+# 2. Backup the database
+docker run --rm \
+    --volume todo-db:/var/lib/mysql \
+    --volume $(pwd):/backup \
+    alpine tar cvf /backup/backup.tar -C /var/lib/mysql .
+    
+# Create/Start the Docker Containers
+docker-compose up
+# end::backup[]
+
+# tag::restore[]
+# 1. Stop/Delete the Docker Containers
+docker-compose down
+
+# 2. Perform the restore
+docker run --rm \
+    --volume todo-db:/var/lib/mysql \
+    --volume $(pwd):/restore \
+    alpine sh -c "rm -rf  /var/lib/mysql/* \
+    && tar xvf /restore/backup.tar -C /var/lib/mysql --strip 1"
+    
+# 3. Create/Start the Docker Containers 
+docker-compose up
+# end::restore[]
+
+# tag::cleanup[]
+# Stop/Delete the Docker Containers as well as the Docker Volume
+docker-compose down -v
+# end::cleanup[]
