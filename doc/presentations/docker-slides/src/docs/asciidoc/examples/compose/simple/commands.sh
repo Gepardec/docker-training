8726a0247337b39173d9d22982bd1d55340c93ec
het <thomas.herzog@gepardec.com>
2018-11-15 08:10:20 +0100

    Finished compose part

--- doc/presentations/docker-slides/src/docs/asciidoc/examples/compose/simple/commands.sh
+++ doc/presentations/docker-slides/src/docs/asciidoc/examples/compose/simple/commands.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+set -x
+set -e
+
+cd $(dirname ${0})
+
 # tag::backup[]
 # 1. Stop/Delete the Docker Containers
 docker-compose down
@@ -9,7 +15,7 @@ docker run --rm \
     alpine tar cvf /backup/backup.tar -C /var/lib/mysql .
     
 # Create/Start the Docker Containers
-docker-compose up
+docker-compose up -d
 # end::backup[]
 
 # tag::restore[]
@@ -24,10 +30,12 @@ docker run --rm \
     && tar xvf /restore/backup.tar -C /var/lib/mysql --strip 1"
     
 # 3. Create/Start the Docker Containers 
-docker-compose up
+docker-compose up -d
 # end::restore[]
 
 # tag::cleanup[]
 # Stop/Delete the Docker Containers as well as the Docker Volume
 docker-compose down -v
 # end::cleanup[]
+
+# Nothing else
