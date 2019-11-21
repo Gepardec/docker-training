262a9421bc9dc07bfb0562d3a4b994cb1bd5037b
Clemens Kaserer <clemens.kaserer@gmail.com>
2019-11-21 13:41:01 +0100

    rebranded docker training slides

--- apps/todo/src/main/resources/db/migration/V1_0_0_1__Create_todo_table.sql
+++
@@ -1,6 +0,0 @@
-create table Todo (
-    id int not null AUTO_INCREMENT,
-    name varchar(250) not null,
-    done boolean DEFAULT FALSE,
-    PRIMARY KEY (id)
-);
--- apps/todo/src/main/resources/db/migration/V1_0_0_2__Insert_todos.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO Todo (name) VALUES ('Learn docker');
